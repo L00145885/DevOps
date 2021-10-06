f769d237af4ba2659941930baf73233e31b57e6a
dave3150 <L00145885@student.lyit.ie>
2021-10-06 08:48:50 +0100

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+   public static void main(String [] args){
+      //Prints "Hello, LYIT from David" as output
+      System.out.println("Hello, LYIT from David");
+   }
+}
